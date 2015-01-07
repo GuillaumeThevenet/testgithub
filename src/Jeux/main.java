f76f02d8f1d54db7f0861076bc84c80dc6fd73f0
Arthur <Arthur@192.168.1.21>
2015-01-07 20:06:06 +0100

    Ajout de grille interface graphique

--- src/Jeux/main.java
+++ src/Jeux/main.java
@@ -1,10 +1,30 @@
 package Jeux;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
 public class main {
 	
 	public static void main (String[] args){
 		
-		//on crée la grille
+		JFrame t = new JFrame();
+		JPanel pan = new JPanel (new GridLayout (10,10));
+		Border blackline = BorderFactory.createLineBorder(Color.black,1); 
+		for(int i = 0; i<100;i++){
+		   JPanel ptest = new JPanel();
+		   ptest.setBorder(blackline);
+		   pan.add(ptest);
+		}
+		pan.setBorder(blackline);
+		t.add(pan);
+		t.setVisible(true);
+		
+		/*//on crée la grille
 		Grille g1 = new Grille(15, 8);
 		
 		
@@ -13,7 +33,7 @@ public class main {
 		g1.placer(15, 1, 'V');
 		
 		//on affiche la grille
-		g1.afficher();
+		g1.afficher();*/
 		
 	}
 
