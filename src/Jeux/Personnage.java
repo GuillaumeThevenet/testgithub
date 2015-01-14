7e170e80fc4c2f85702920ec0157c51fc7875955
AHMED ECHOUI <AHMED ECHOUI@172.16.233.233>
2015-01-14 16:06:48 +0100

    ok

--- src/Jeux/Personnage.java
+++ src/Jeux/Personnage.java
@@ -1,53 +1,50 @@
 package Jeux;
 
 public class Personnage {
-	
-	//Attributs
-	
-	private int position; //Vie du perso
-	private int vitesse;
-	private String color;
-	
-	//Constructeurs
-	
+
+	// Attributs
+
+	private int position; // position du perso
+	private int vitesse;// vitesse du perso
+	private String color;// couleur du perso
+
+	// Constructeurs
+
 	public Personnage(int v, int p, String c) {
-		position=p;
-		vitesse=v;
-		color=c;
-		
+		position = p;
+		vitesse = v;
+		color = c;
+
 	}
-	
-	//Accesseurs
-	
+
+	// Accesseurs
+
 	public int getPosition() {
-		return position; //Renvoie la position
+		return position; // Renvoie la position
 	}
 
 	public int getVitesse() {
 		return vitesse;
 	}
-	
-	public String getColor(){
+
+	public String getColor() {
 		return color;
 	}
-	
-	//Setters
-	
+
+	// Setters
+
 	public void setPosition(int p) {
-		position = p; //Définie la position
+		position = p; // Définie la position
 	}
-	
-	public void setVitesse (int v) {
+
+	public void setVitesse(int v) {
 		vitesse = v;
 	}
-	
-	public void setColor(String c){
+
+	public void setColor(String c) {
 		color = c;
 	}
 }
-	
-	//Services
-	
 
-
+// Services
 
